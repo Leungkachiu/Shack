0b8f8ce159823ebfd4b76226685680b166673bf6
Jiazhao Liang <jliang@flexport.com>
2024-12-02 15:32:57 +0800

    change dependency

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import slack
+from slack_sdk import WebClient
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -8,13 +8,12 @@ from slackeventsapi import SlackEventAdapter
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-
+client = WebClient(token=os.environ['SLACK_TOKEN'])
 
 app = Flask(__name__)
 print(os.environ['SIGNING_SECRET'])
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],"/slack/events",app)
-
+BOT_ID = client.api_call("auth.test")['user_id']
 
 @slack_event_adapter.on("message")
 def message(payload):
@@ -23,8 +22,9 @@ def message(payload):
     event = payload.get("event",{})
     channel = event.get("channel")
     text = event.get("text")
-    user = event.get("user")
-    client.chat_postMessage(channel=channel, text=text)
+    user_id = event.get("user")
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel, text=text)
 # client.chat_postMessage(channel="#test", text="Hello World!")
 
 if __name__  == "__main__":
